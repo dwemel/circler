ef3f28b7013a37315292bba00bc73ace5534f1e5
dwemel <58175993+dwemel@users.noreply.github.com>
2020-01-05 15:01:18 +0100

    Update writeFile.mjs

--- src/writeFile.mjs
+++ src/writeFile.mjs
@@ -4,7 +4,6 @@ import getFileDate from './getFileDate.mjs';
 
 export default (dir, date, name, buffer) => {
   var name = name.replace(" ","_");
-  var buffer = buffer.replace(" ","_");
   const datePath = getFileDate(date);
   const fullPath = resolve(dir, datePath, name);
   const dirName = dirname(fullPath);
@@ -12,7 +11,7 @@ export default (dir, date, name, buffer) => {
   return new Promise((resolve, reject) => {
     try {
       console.log(`Saving '${fullPath}' to disk...`);
-      writeFile(fullPath, buffer, err => {
+      writeFile(fullPath, err => {
         if (err) return void reject(err);
         resolve();
       })
